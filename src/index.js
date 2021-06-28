9a23b244f398dbd124e0a1c6eaab0380067bf0ef
caballo <imanol.l.p@hotmail.com>
2021-06-28 11:19:57 +0200

    Re-refactored, fixed localStorage issue

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import { createProject, createTask, createNote } from './modules/objects';
 import { storage } from './modules/storage';
 
+
+function makeDummys() {
 createTask('testTitle2', 'testDesc2', '10/3/2007', 2);
 createTask('testTitle', 'testDesc', '10/3/2006', 1);
 
 const testTask2 = storage.tasks[0];
 const testTask = storage.tasks[1];
 
-
+createProject('third ', 'desc')
+createProject('anocher prochecht', 'And the description')
 createProject('Project2', 'This is a description');
 storage.projects[0].tasks.push(testTask, testTask2)
 
-//createNote('Note to self', 'Dont fart in public');
+createNote('Note to self', 'Dont fart in public');
+}
 
 
-console.log(storage.projects[0]);
-console.log(storage.projects[0].tasks)
-console.log(storage.notes.length === 0 ? 'yes' : 'no');
-console.log(storage.notes)
 
-storage.loadArrays();
+console.log('All projects:');
+console.log(storage.projects);
+console.log('All tasks:');
+console.log(storage.tasks)
+console.log('All notes:');
+console.log(storage.notes);
+console.log('LocalStorage:');
+console.log(window.localStorage);
+
+setTimeout(() => makeDummys(), 0)
+
--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,54 +1,46 @@
 const storage = (() => {
-
 	let projects = [];
 	let tasks = [];
 	let notes = [];
 
+	const save = (object) => {
+		saveInArrays(object.type, object);
+		localStore();
+	}
 
-	const localStore = (type, object) => {
-		window.localStorage.setItem(type, JSON.stringify(object));
-	};
+	const saveInArrays = (type, object) => {
+		(type === 'project') ? projects.push(object) :
+		(type === 'task') ? tasks.push(object) :
+		(type === 'note') ? notes.push(object) : 
+		console.log('Error saving object');
+	}
+
+	const localStore = () => {
+		window.localStorage.setItem('project', JSON.stringify(projects));
+		window.localStorage.setItem('task', JSON.stringify(tasks));
+		window.localStorage.setItem('note', JSON.stringify(notes));
+	}
+
+
+	const loadArrays = () => {
+			projects = pullAllFromStorage('project')
+			tasks = pullAllFromStorage('task')
+			notes = pullAllFromStorage('note')
+	}
 
 	//Add index to parameters so you can choose which to pull??
 	const pullAllFromStorage = (type) => {
 		const parsed = JSON.parse(window.localStorage.getItem(type));
 		return parsed;
 	};
-
-	const save = (object) => {
-
-		localStore(object.type, object);
-		saveInArrays(object.type, object)
-	}
-
-	const saveInArrays = (type, object) => {
-		
-		type === 'project' ? projects.push(object) :
-		type === 'task' ? tasks.push(object) :
-		type === 'note' ? notes.push(object) : 
-		console.log('Error saving object');
-		
-	}
-
-	const loadArrays = () => {
-		if (projects.length === 0) {
-			projects.push(pullAllFromStorage('project'))
-		}
-		if (tasks.length === 0) {
-			tasks.push(pullAllFromStorage('task'))
-		}
-		if (notes.length === 0) {
-			notes.push(pullAllFromStorage('note'))
-		}
-
-	}
+	
 
 	return {
 		projects,
 		tasks,
 		notes,
 		save,
-		loadArrays
+		loadArrays,
 	};
 })();
 
